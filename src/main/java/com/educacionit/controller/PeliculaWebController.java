bbc9b3dd3e64500a2d49fe55960f164b6837af5a
Emmanuel Greco <emma.greco.96@gmail.com>
2024-07-10 04:14:22 -0300

    PeliculaApiController y PeliculaWebController

---
+++ src/main/java/com/educacionit/controller/PeliculaWebController.java
@@ -0,0 +1,16 @@
+package com.educacionit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/peliculas")
+public class PeliculaWebController {
+	
+	@GetMapping
+	public String getIndexPage(Model model) {
+		return "index";
+	}
+}
