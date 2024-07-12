af105d693a29325a8a549f79b4990810be8c5a11
Emmanuel Greco <emma.greco.96@gmail.com>
2024-07-11 21:28:39 -0300

    Agrego ApplicationConfig, SecurityConfig y jwt

---
+++ src/main/java/com/educacionit/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.educacionit.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.educacionit.jwt.JwtAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	@Autowired
+	private AuthenticationProvider authProvider;
+
+	public SecurityConfig() {
+		super();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(authRequest -> authRequest
+					.requestMatchers("/auth/**", "/login").permitAll()
+					.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+					.requestMatchers("/css/**", "/js/**").permitAll()
+					.requestMatchers("/api/v1/peliculas", "/api/v1/peliculas/search").permitAll()
+					.requestMatchers("/favicon.ico", "/").permitAll().requestMatchers("/admin").hasRole("ADMIN")
+					.requestMatchers("/user").hasRole("SOCIO").anyRequest().authenticated())
+			.sessionManagement(sessionManager -> sessionManager
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authenticationProvider(authProvider)
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.exceptionHandling(exceptionHandler -> exceptionHandler
+					.accessDeniedHandler(accessDeniedHandler()))
+		.build();
+	}
+
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler() {
+		return (request, response, accessDeniedException) -> {
+			response.sendRedirect("/error/403");
+		};
+	}
+}
